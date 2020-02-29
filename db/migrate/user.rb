96bb3aeb2b1266804ebd4fd32ca14811aeaf4707
ryan m <contact@ryanmeek.com>
2020-02-29 19:34:35 +0000

    Automatically backed up by Learn

--- db/migrate/user.rb
+++ db/migrate/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Migration
   def change
-    create_table(:users) { |t| t.string :username; t.string :password; t.decimal :balance}
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
   end
 end
